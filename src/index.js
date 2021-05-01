cd765efac45e9c64cd19d8761c363033f1acd6c7
Mustafa Yavaş <43928058+MustafaYavas@users.noreply.github.com>
2021-05-01 09:32:57 +0200

    Removed comments

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const {addUser, removeUser, getUser, getUsersInRoom} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);    // server tarafında socket.io'yu kullanabilmek için. client tarafında kullanmak için client tarafında bir daha yüklemek gerek
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public")
@@ -16,9 +16,8 @@ const publicDirectoryPath = path.join(__dirname, "../public")
 app.use(express.static(publicDirectoryPath));
 
 
-// Buradaki socket parametresi socket.io sunucusundan gelir. Client tarafından bir bağlantı kurulduğunda 
-// "connection" eventi, socket serverı tarafından yayınlanır ve bu bağlantı argüman olarak iletilir.
-io.on("connection", (socket) => {   // her yeni bağlantıda bu metod çalışacak
+
+io.on("connection", (socket) => {   
     console.log("New WebSocket connected");    
 
     socket.on("join", (options, cb) => {
@@ -41,7 +40,7 @@ io.on("connection", (socket) => {   // her yeni bağlantıda bu metod çalışac
     })
 
     socket.on("sendMessage", (message, cb) => {
-        const user = getUser(socket.id); //user yerine destructuring kullanabilirdik. Yani const {room} = getUser(socket.id);
+        const user = getUser(socket.id); 
         const filter = new Filter();
 
         if(filter.isProfane(message)){
@@ -55,7 +54,7 @@ io.on("connection", (socket) => {   // her yeni bağlantıda bu metod çalışac
 
 
     socket.on("sendLocation", (coords, cb) => {
-        const user = getUser(socket.id);    //user yerine destructuring kullanabilirdik. Yani const {room} = getUser(socket.id);
+        const user = getUser(socket.id);
         io.to(user.room).emit("locationMessage", generateLocationMessage(user.username,`https://google.com/maps?q=${coords.lat},${coords.lon}`));
         cb();
     })
@@ -74,17 +73,6 @@ io.on("connection", (socket) => {   // her yeni bağlantıda bu metod çalışac
     })
 
 })
-
-// Özet;
-// server (emit) --> client receive - countUpdated
-// client (emit) --> server receive - increment
-// server, countUpdated adındaki eventi yayar ve client tarafı bu eventi alır. Daha sonra client tarafı da bir event yayar
-// ve bu defa server tarafı bu eventi alır
-// socket.on() --> karşılıklı olarak gelen eventleri almak için kullanılır
-// socket.emit() --> karşılıklı olarak event göndermek için kullanılır. 
-//                   Bu metod server tarafından kullanıldığında sadece bir ilgili client'a eventi iletir
-//io.emit() --> Bu metod kullanıldığında ise oluşturulan event diğer TÜM mevcut olan bağlantılara iletilir.
-
 
 server.listen(port, () =>{
     console.log(`Server is up on port ${port}!`);
